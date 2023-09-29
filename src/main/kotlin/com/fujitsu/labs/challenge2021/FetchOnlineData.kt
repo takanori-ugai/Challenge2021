6097904564269e126da2bd4cc23f249c5aeafb38
Takanori Ugai <ugai@fujitsu.com>
2023-09-29 18:03:57 +0900

    small optimization

--- src/main/kotlin/com/fujitsu/labs/challenge2021/FetchOnlineData.kt
+++ src/main/kotlin/com/fujitsu/labs/challenge2021/FetchOnlineData.kt
@@ -5,6 +5,9 @@ import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue
 import org.wikidata.wdtk.datamodel.interfaces.StatementDocument
 import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher
 
+/**
+ * The main function that fetches online data and prints class definition.
+ */
 fun main() {
     val aa = FetchOnlineData()
     val tt = aa.getType("Douglas Adams")
@@ -12,15 +15,28 @@ fun main() {
     aa.printClassDefinition(tt)
 }
 
+/**
+ * A class to fetch online data from Wikibase.
+ */
 class FetchOnlineData {
 
     private val wbdf: WikibaseDataFetcher = WikibaseDataFetcher.getWikidataDataFetcher()
 
+    /**
+     * Gets the ID of an entity by its name.
+     * @param str The name of the entity.
+     * @return The ID of the entity.
+     */
     fun getId(str: String): String {
         val q43 = wbdf.searchEntities(str, "en", 10)
         return if (q43.isEmpty()) { "" } else { q43.first().entityId }
     }
 
+    /**
+     * Gets the type of an entity by its name.
+     * @param str The name of the entity.
+     * @return The type of the entity.
+     */
     fun getType(str: String): String {
         val q42 = wbdf.getEntityDocument(getId(str))
         if (q42 is StatementDocument) {
@@ -35,6 +51,11 @@ class FetchOnlineData {
         return ""
     }
 
+    /**
+     * Gets the label of an entity by its ID.
+     * @param str The ID of the entity.
+     * @return The label of the entity.
+     */
     fun getLabel(str: String): String {
         val q42 = wbdf.getEntityDocument(str)
         return if (q42 is ItemDocument) {
@@ -43,23 +64,25 @@ class FetchOnlineData {
         } else { "" }
     }
 
+    /**
+     * Gets a list of classes for an entity by its ID.
+     * @param str The ID of the entity.
+     * @return A list of classes for the entity.
+     */
     fun getClassList(str: String): List<String> {
-        val ret = mutableListOf<String>()
-        val q5 = wbdf.getEntityDocument(str)
-        if (q5 is StatementDocument) {
-            val group = q5.findStatementGroup("P279")
-            if (group != null) {
-                for (i in group.statements) {
-                    val state = i.value
-                    if (state is ItemIdValue) {
-                        ret.add(state.id)
-                    }
-                }
-            }
+        val document = wbdf.getEntityDocument(str)
+        return if (document is StatementDocument) {
+            val group = document.findStatementGroup("P279")
+            group?.statements?.mapNotNull { it.value as? ItemIdValue }?.map { it.id } ?: emptyList()
+        } else {
+            emptyList()
         }
-        return ret
     }
 
+    /**
+     * Prints the class definition of an entity by its ID.
+     * @param str The ID of the entity.
+     */
     fun printClassDefinition(str: String) {
         val label = getLabel(str)
         val label2 = label.replace(" ", "_")
